15e9315d0c8df47656f89bee90de0f6c66a4e5da
soda-cheng <b1242921098@gmail.com>
2026-02-23 17:18:17 +0800

    save local changes before merge

--- projects/ood_eval/config/vit_ood-eval_toy-example_imagenet-c.py
+++
@@ -1,4 +0,0 @@
-_base_ = 'mmcls::resnet/resnetv1c50_8xb32_in1k.py'  # can be your own config
-
-test_dataloader = dict(dataset=dict(data_root='data/imagenet-c'))
-test_evaluator = dict(type='CorruptionError')
